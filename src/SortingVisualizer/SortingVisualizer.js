8b4aa309d06388c08f31e7fc0c644f50500c9c11
“Ryan” <pagsmanagement@gmail.com>
2022-10-02 00:39:50 -0600

    Tweaks

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -16,7 +16,7 @@ function SortingVisualizer() {
     const resetArray = () => {
         const array = []
         for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-            array.push(randomNumber(25, 500))
+            array.push(randomNumber(25, 400))
         }
         setArray(array)
     }
